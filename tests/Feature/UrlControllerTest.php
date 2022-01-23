18f933dcef2d7405f78f32537e3edd5daaf3f4db
Rustam Amirov <r.amirov@dsserv.ru>
2022-01-23 23:02:23 +0300

    added tests

--- tests/Feature/UrlControllerTest.php
+++ tests/Feature/UrlControllerTest.php
@@ -4,13 +4,14 @@ namespace Tests\Feature;
 
 use Tests\TestCase;
 use App\Models\Url;
+use Illuminate\Support\Facades\DB;
 
 class UrlControllerTest extends TestCase
 {
     protected function setUp(): void
     {
         parent::setUp();
-        $m = Url::factory()->count(2)->make()->toArray();
+        Url::factory()->count(2)->make()->toArray();
     }
 
     public function testIndex()
@@ -24,10 +25,23 @@ class UrlControllerTest extends TestCase
         $factoryData = Url::factory()->make()->toArray();
         $data = ['url' => ['name' => $factoryData['name']]];
         $response = $this->post(route('urls.store'), $data);
-        //$response->assertRedirect(route('urls.show'));
+
+        $scheme = parse_url($factoryData['name'], PHP_URL_SCHEME);
+        $host = parse_url($factoryData['name'], PHP_URL_HOST);
+        $url = $scheme . '://' . $host;
+        $url = DB::table('urls')->where('name', $url)->first();
+
+        $response->assertRedirect(route('urls.show', $url->id));
         $response->assertSessionHasNoErrors();
 
-        //$response = $this->get(route('urls.index'))->assertSeeText($factoryData['name']);
-        //$this->assertDatabaseHas('urls', ['name' => $factoryData['name']]);
+        $response = $this->get(route('urls.index'))->assertSeeText($url->name);
+        $this->assertDatabaseHas('urls', ['name' => $url->name]);
+    }
+
+    public function testShow()
+    {
+        $url = DB::table('urls')->first(); 
+        $response = $this->get(route('urls.show', $url->id));
+        $response->assertOk();
     }
 }
